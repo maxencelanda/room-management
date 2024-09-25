fcaa148971bd018f58507c64b6eb330da79b4794
max <maxence.landa@gmail.com>
2024-09-25 09:26:41 +0200

    create user api

--- back/app.js
+++ back/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const db = require("./db")
+const { createHash } = require('crypto')
 var cors = require('cors');
 const app = express()
 const port = 3000
@@ -29,13 +30,60 @@ app.get('/salles/all', (req, res) => {
 })
 
 app.get('/salles/:salleId', (req, res) => {
-    db.query(`SELECT * FROM Salle WHERE idSalle = ${req.params.salleId}`, (err, results) => {
+    db.query(`SELECT * FROM Salle WHERE idSalle = ?`, [req.params.salleId], (err, results) => {
         if (err) {
           console.error('Error executing query: ' + err.stack);
           res.status(500).send('Error fetching users');
           return;
         }
         res.json(results);
+    });
+})
+
+app.get('/creneaux/all', (req, res) => {
+    db.query(`SELECT * FROM Creneau`, (err, results) => {
+        if (err) {
+          console.error('Error executing query: ' + err.stack);
+          res.status(500).send('Error fetching users');
+          return;
+        }
+        res.json(results);
+    });
+})
+
+app.get('/reservation/:salleId', (req, res) => {
+    db.query(`SELECT * FROM Reservation WHERE idSalle = ? AND dateReservation >= DATE(NOW())`, [req.params.salleId], (err, results) => {
+        if (err) {
+          console.error('Error executing query: ' + err.stack);
+          res.status(500).send('Error fetching users');
+          return;
+        }
+        res.json(results);
+    });
+})
+
+app.post('/reservation/create', (req, res) => {
+    const data = req.body
+    db.query(`INSERT INTO Reservation VALUES(NULL, ?, ?, ?, ?, ?)`, [data.nom, data.description, data.dateReservation, data.idSalle, data.idCreneau], (err, results) => {
+        if (err) {
+          console.error('Error executing query: ' + err.stack);
+          res.status(500).send('Error fetching users');
+          return;
+        }
+        res.json({"message": "success"});
+    });
+})
+
+app.post('/utilisateur/create', (req, res) => {
+    const data = req.body
+    const mdp = createHash("sha256").update(data.mdp).digest("hex")
+    db.query(`INSERT INTO Utilisateur VALUES(NULL, )`, [data.nom, mdp], (err, results) => {
+        if (err) {
+          console.error('Error executing query: ' + err.stack);
+          res.status(500).send('Error fetching users');
+          return;
+        }
+        res.json({"message": "success"});
     });
 })
 
--- front/components/SalleDetail.js
+++ front/components/SalleDetail.js
@@ -4,11 +4,19 @@ import { useEffect, useState } from 'react';
 
 export default function SalleDetail({ navigation, route }) {
     const [salle, setSalle] = useState([]);
+    const [reservations, setReservations] = useState([])
+    const [creneaux, setCreneaux] = useState([])
+    const today = new Date().toJSON().slice(0, 10);
+    const todayFormat = new Date().toDateString()
 
     useEffect(() => {
       const fetchData = async () => {
         const salleResponse = await axios.get(`http://10.60.137.63:3000/salles/${route.params.salleId}`)
         setSalle(salleResponse.data[0]);
+        const reservationResponse = await axios.get(`http://10.60.137.63:3000/reservation/${route.params.salleId}`)
+        setReservations(reservationResponse.data);
+        const creneauResponse = await axios.get(`http://10.60.137.63:3000/creneaux/all`)
+        setCreneaux(creneauResponse.data);
       }
       fetchData()
   }, [])
@@ -18,9 +26,27 @@ export default function SalleDetail({ navigation, route }) {
         <View style={styles.header}>
           <Text style={styles.title}>{salle["nom"]}</Text>
         </View>
-        <View>
-          
-        </View>
+          {
+            creneaux.map((creneau, i) => (
+              <View>
+              <Text key={i} style={styles.salle}>{creneau["horaire"]}</Text>
+              {
+                reservations.filter(reservation => today == reservation["dateReservation"] && creneau["idCreneau"] == reservation["idCreneau"]).length > 0 ?
+                reservations.map((reservation, x) => 
+                  <Text key={x} style={styles.salle}>Réservé</Text>
+                )
+                :
+                <Text key={i} style={styles.salle}>Disponible</Text>
+              }
+              </View>
+            )
+          )}
+          {/*reservations.map((reservation, i) => 
+            today == reservation["dateReservation"] ? 
+            <Text key={i} style={styles.salle}>Ajd</Text>
+            : <Text key={i} style={styles.salle}>Pas ajd ({reservation["dateReservation"]} vs {today})</Text>
+          )
+          */}
     </View>
   )
 }
